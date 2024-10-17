fe061e08edd72cb7eed3940641d91c853f40a780
Binondi Borthakur <141143315+Binondi@users.noreply.github.com>
2024-10-17 10:20:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 join_db = JoinReqs
-emoji_list = ["🔥", "⚡", "😍", "🥳", "🎉", "❤️", "❤️‍🔥"]
+emoji_list = ["🔥", "⚡", "😍", "🥳", "🎉", "❤️", "❤️‍🔥","👀"]
 
 @Client.on_message(filters.command("start") & filters.incoming)
 # Define your list of emojis
